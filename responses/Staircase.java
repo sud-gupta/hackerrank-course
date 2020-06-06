b5640152db3b17a1eb89e12d1aa04bf3539897cf
sud-gupta <66489983+sud-gupta@users.noreply.github.com>
2020-06-06 13:43:50 +0530

    Create Staircase.java

---
+++ responses/Staircase.java
@@ -0,0 +1,11 @@
+static void staircase(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
